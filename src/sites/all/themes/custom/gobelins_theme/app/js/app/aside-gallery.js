4185de5c0d4031f9d6b0d35873f2fc7ae46c4b22
Tony Cabaye <tocab@smile.fr>
2015-05-28 18:37:56 +0200

    implements AJAX call for aside gallery.

--- src/sites/all/themes/custom/gobelins_theme/app/js/app/aside-gallery.js
+++ src/sites/all/themes/custom/gobelins_theme/app/js/app/aside-gallery.js
@@ -3,10 +3,12 @@
 
   /* Require plugins */
   var $ = require('jquery');
+  var tmpl = require('../jstemplates/aside-gallery__ajax.js');
   require('../../../bower_components/jquery-contenttoggle/jquery.contenttoggle.js');
 
   $(function(){
-    var $gallery;
+    var $gallery, doneCallback;
+    var $more = $('.js-masonry--aside-gallery__more');
     
     /* Open/close gallery. */
     $('.with-gallery').contentToggle({
@@ -26,5 +28,40 @@
         columnWidth: '.aside-gallery__item--1'
       });
     });
+    
+    /* AJAX call */
+    if (window.gobelinsSettings &&
+        window.gobelinsSettings.asideGallery &&
+        window.gobelinsSettings.asideGallery.url &&
+        $more.length > 0) {
+      
+      doneCallback = function(data){
+        var html, $items;
+        
+        if (!data.more) {
+          $more.hide();
+          $more.off('click');
+        }
+        
+        if (typeof data.items == 'object') {
+          html = tmpl['aside-gallery__ajax.html'](data);
+          $items = $(html).filter('.js-masonry--aside-gallery__item');
+          
+          $gallery.append($items);
+          $gallery.masonry('appended', $items);
+          
+          $items.imagesLoaded(function() {
+            $gallery.masonry();
+          });
+        }
+      };
+
+      $more.on('click', function(){
+        $.ajax({
+          url: window.gobelinsSettings.asideGallery.url,
+          dataType: 'json'
+        }).done(doneCallback);
+      });
+    }
   });
 })();
