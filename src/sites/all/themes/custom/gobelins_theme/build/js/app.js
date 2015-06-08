6c9bee261a518e316d151f4d6074bb75a1e9f536
Tony Cabaye <tocab@smile.fr>
2015-06-08 09:30:06 +0200

    Commit build.

--- src/sites/all/themes/custom/gobelins_theme/build/js/app.js
+++ src/sites/all/themes/custom/gobelins_theme/build/js/app.js
@@ -842,29 +842,35 @@ require=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof requ
   var $ = require('jquery');
 
   $(function(){
+    var getPositions;
     var $window = $(window);
     var $root = $('html, body');
     var $container = $('.js-sticky-menu__container');
     var $scroll = $('.js-sticky-menu__scroll');
     var $sections = $('.js-sticky-menu__section');
-    var $titles = $('.js-sticky-menu__title');
     var offset = $('.js-header-sticky').height() + $container.height();
     var positions = [];
     var $links = $();
+
+    // Initializes positions.
+    getPositions = function(){
+      $sections.each(function(index){
+        var $section = $sections.eq(index);
+        positions[index] = {
+          top: $section.offset().top - offset,
+          height: $section.outerHeight()
+        };
+      });
+    };
 
     $('.js-sticky-menu').sticky({
       wrapperClassName: 'sticky-menu__sticky-wrapper',
       topSpacing: 80
     });
 
-    $titles.each(function(index){
+    $sections.each(function(index){
       var $link;
-      var $title = $titles.eq(index);
-      var title = $title.data('title');
-
-      if (!title) {
-        title = $title.text();
-      }
+      var title = $sections.eq(index).data('title');
 
       $link = $(document.createElement('a'))
         .addClass('sticky-menu__link')
@@ -878,38 +884,33 @@ require=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof requ
       $links = $links.add($link);
     });
 
-    window.setTimeout(function(){
-      // Initializes positions.
-      $sections.each(function(index){
-        var $section = $sections.eq(index);
-        positions[index] = {
-          top: $section.offset().top - offset,
-          height: $section.outerHeight()
-        };
-      });
-    }, 0);
-
+    window.setTimeout(getPositions, 0);
 
     $window.on('scroll', function(){
       var scrollTop = $window.scrollTop();
       var activeIndex = null;
 
-      // Find active link.
-      positions.forEach(function(position, index){
-        if (scrollTop >= position.top && scrollTop < position.top + position.height) {
-          activeIndex = index;
-        }
-      });
+      // Update active link.
+      if ($sections.length > 0) {
+        positions.forEach(function(position, index){
+          if (scrollTop >= position.top && scrollTop < position.top + position.height) {
+            activeIndex = index;
+          }
+        });
 
-      // Set active link.
-      $links.removeClass('is-active');
-      if (activeIndex !== null) {
-        $links.eq(activeIndex).addClass('is-active');
+        $links.removeClass('is-active');
+        if (activeIndex !== null) {
+          $links.eq(activeIndex).addClass('is-active');
+        }
       }
 
       // Update scroll bar.
       $scroll.width((scrollTop / ($root.height() - $window.height()) * 100) + '%');
     });
+
+    if ($sections.length > 0) {
+      $window.on('scroll', getPositions);
+    }
   });
 })();
 
@@ -2294,7 +2295,10 @@ Array.prototype.move = function (oldIndex, newIndex) {
     // Bind native events on triggers.
     this.$triggers.on(eventName + namespaces, function(event){
       event.preventDefault();
-      event.timeStamp && this.toggle(null, event);
+      if (!event.originalEvent.mozInputSource ||
+          event.originalEvent.mozInputSource !== MouseEvent.MOZ_SOURCE_KEYBOARD) {
+        this.toggle(null, event);
+      }
     }.bind(this));
     this.$triggers.on('keydown' + namespaces, function(event){
       if (event.keyCode == ENTER_KEY_CODE || event.keyCode == SPACE_KEY_CODE) {
